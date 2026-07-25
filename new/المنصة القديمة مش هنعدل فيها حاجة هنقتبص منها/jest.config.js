9ec8a5d538dd6cc4b17b477fda773d9e529a898c
Hassan <hsnsh@users.noreply.github.com>
2026-07-25 06:49:02 +0300

    chore: exclude legacy platform directory from git tracking

--- new/المنصة القديمة مش هنعدل فيها حاجة هنقتبص منها/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
-  testMatch: ['**/*.test.js'],
-  verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/tests/setupEnv.js'],
-  forceExit: true,
-};
--- new/المنصة القديمة مش هنعدل فيها حاجة هنقتبص منها/server/routes/chatCustomers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authenticate = require('../middleware/authenticate');
-const chatCustomersController = require('../controllers/chatCustomersController');
-
-const router = express.Router();
-
-router.get('/', authenticate, chatCustomersController.listCustomers);
-
-module.exports = router;
